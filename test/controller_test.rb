9fd04d50ce99e879dd63d38008b3115566d04034
Matthew Wear <mwear@newrelic.com>
2015-05-01 14:35:25 -0700

    Rename FakeController=>TestController

--- test/controller_test.rb
+++ test/controller_test.rb
@@ -2,10 +2,10 @@ require 'minitest_helper'
 
 class ControllerTest < MiniTest::Test
 
-  class FakeController < Rims::Controller
-    description "Fake Controller"
+  class TestController < Rims::Controller
+    description "Test Controller"
 
-    route :post, "/fake"
+    route :post, "/test"
 
     action do
       "done..."
@@ -21,17 +21,17 @@ class ControllerTest < MiniTest::Test
   end
 
   def test_controller_has_description
-    assert_equal "Fake Controller", FakeController.description
+    assert_equal "Test Controller", TestController.description
   end
 
   def test_route_adds_an_endpoint
-    endpoint = FakeController.endpoints.first
+    endpoint = TestController.endpoints.first
     assert_equal "POST", endpoint.verb
-    assert_equal "/fake", endpoint.path
+    assert_equal "/test", endpoint.path
   end
 
   def test_controllers_are_registered
-    assert_includes Rims::Controller.registry, FakeController
+    assert_includes Rims::Controller.registry, TestController
   end
 
   def test_controllers_can_render_statuses
